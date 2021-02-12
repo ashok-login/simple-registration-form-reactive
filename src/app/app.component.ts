69c859e5b536637f08b97af3a9623f1e6f56cb4b
Ashok Kumar Kottagundu <akottagundu@tollplus.com>
2021-02-12 17:04:38 +0530

    Session discussions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { forbiddenNameValidator } from './shared/user-name.validator';
 
 @Component({
   selector: 'app-root',
@@ -7,17 +8,38 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  registrationForm = new FormGroup({
-    userName : new FormControl('John'),
-    password : new FormControl(''),
-    confirmPassword : new FormControl(''),
-    address : new FormGroup({
-      city : new FormControl(''),
-      state : new FormControl(''),
-      postalCode : new FormControl('')
+  // Commented for Associate's reference.
+  // registrationForm = new FormGroup({
+  //   userName : new FormControl('John'),
+  //   password : new FormControl(''),
+  //   confirmPassword : new FormControl(''),
+  //   address : new FormGroup({
+  //     city : new FormControl(''),
+  //     state : new FormControl(''),
+  //     postalCode : new FormControl('')
+  //   })
+  // });
+
+  get userName() {
+    return this.registrationForm.get('userName');
+  }
+
+  get password() {
+    return this.registrationForm.get('password');
+  }
+
+  constructor(private fb: FormBuilder) {}
+  registrationForm = this.fb.group({
+    userName: ['', [Validators.required, Validators.minLength(3),
+                    forbiddenNameValidator(/admin/)]],
+    password: ['',[forbiddenNameValidator(/password/)]],
+    confirmPassword: [''],
+    address: this.fb.group({
+      city: [''],
+      state: [''],
+      postalCode: ['']
     })
   });
-
   setValueToLoadApiData() {
     this.registrationForm.setValue({
       userName : 'Bruce',
